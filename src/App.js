a950bd552d2844c94159196355d0d91c5b3d80dc
OptiCodeSr <148829872+OptiCodeSr@users.noreply.github.com>
2024-11-19 11:44:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import { loader as weatherLoader } from "./pages/Root";
+import Spinner from "./components/Spinner";
+
+// * Lazy loading
+const DailyWeatherDetailPage = lazy(() => import("./pages/DailyWeatherDetail"));
+const SearchedCityDetailPage = lazy(() => import("./pages/SearchedCityDetail"));
+
+// * App routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    loader: weatherLoader,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "daily-weather/:dailyWeatherDetail",
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <DailyWeatherDetailPage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+  {
+    path: "search/:searchedCityDetail",
+    element: (
+      <Suspense fallback={<Spinner />}>
+        <SearchedCityDetailPage />
+      </Suspense>
+    ),
+    loader: ({ params }) =>
+      import("./pages/SearchedCityDetail").then((module) =>
+        module.loader({ params })
+      ),
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "daily-weather/:dailyWeatherDetail",
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <DailyWeatherDetailPage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
